79c49582629d6717dc86eb080d0f8b6cdfaed73c
Guilherme Paes <paes.guilherme@hotmail.com>
2022-12-30 11:01:14 -0300

    fixed positions, clicks and width/height  errors

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,6 @@ const links = document.getElementById("links")
 menuBtn.addEventListener('click', toggleMenu);
 menuBtn.addEventListener('touchstart', toggleMenu);
 links.addEventListener('click', toggleMenu);
-links.addEventListener('touchstart', toggleMenu);
 
 function toggleMenu(event){
    if(event.type === 'touchstart'){
