856c21705a2628e180c8e37e322fe77cd3edc694
Guilherme Paes <paes.guilherme@hotmail.com>
2022-12-31 01:09:14 -0300

    fixed footer colors and links

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -3,8 +3,7 @@ let projects = document.getElementById('projects')
 
         for(i = 0; i < item.length; i++){
             projName = item[i].id;
-            let thumbNail = `<img src="img/thumb_${projName}.jpg">`
-            let title = `<img src="img/thumb_${projName}.jpg">`
+            let thumbNail = `<img src="img/thumbs/thumb_${projName}.jpg">`
 
             item[i].innerHTML += thumbNail;
         }
